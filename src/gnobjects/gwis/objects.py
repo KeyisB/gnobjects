ef1cf43db69ad80ada467a3f9d870a4eb442fc3e
KeyisB <keyisbp@gmail.com>
2026-05-17 20:06:28 +0300

    Update version to 0.0.67 in pyproject.toml; add GWISObject class for object ID handling and validation

---
+++ src/gnobjects/gwis/objects.py
@@ -0,0 +1,37 @@
+from .values import tablex_gwis_object_types_int_to_str, table_gwis_object_types_int_ranges
+
+
+class GWISObject:
+    def __init__(self, gwisid: int, type: int):
+        self.gwisid = gwisid
+        self.type = type
+        self.__object_id = None
+
+    @property
+    def objectId(self):
+        """
+        # Получение id объекта
+        ID объекта кодируется в gwisid
+
+        Поддерживается для:
+
+        - `8` `federation`
+        - `9` `region`
+        """
+
+        if self.__object_id is not None:
+            return self.__object_id
+
+        _r = table_gwis_object_types_int_ranges.get(self.type)
+        if _r is None:
+            if self.type not in tablex_gwis_object_types_int_to_str:
+                raise Exception(f'Unknown object type {self.type}')
+            else:
+                raise Exception(f'Object type {tablex_gwis_object_types_int_to_str[self.type]} has no supported object_id')
+        
+        object_id = self.gwisid - _r[0]
+        if object_id < 0 or object_id >= _r[1] - _r[0]:
+            raise Exception(f'Invalid object_id {self.gwisid} for object type {tablex_gwis_object_types_int_to_str[self.type]}')
+
+        self.__object_id = object_id
+        return object_id
--- src/gnobjects/gwis/values.py
+++ src/gnobjects/gwis/values.py
@@ -1,4 +1,4 @@
-tablex_gwis_object_types_int_to_str = {
+tablex_gwis_object_types_int_to_str: dict[int, str] = {
     0: 'gbn',
     2: 'user',
     3: 'company',
@@ -7,14 +7,26 @@ tablex_gwis_object_types_int_to_str = {
     6: 'service',
     7: 'doo', # Distributed Ownership Object
     8: 'federation',
+    9: 'region',
     255: 'freenet' # access from web2
 }
+gwis_object_types_int_object_id_start: int = 100_001
+table_gwis_object_types_int_max_object_id: dict[int, int] = {
+    8: 2 ** 16,
+    9: 2 ** 32
+}
+
+table_gwis_object_types_int_ranges: dict[int, tuple[int, int]] = {
+}
+
+for o, m in table_gwis_object_types_int_max_object_id.items():
+    table_gwis_object_types_int_ranges[o] = (
+        gwis_object_types_int_object_id_start, 
+        gwis_object_types_int_object_id_start + m
+    )
+    gwis_object_types_int_object_id_start += m
 
 tablex_gwis_object_types_str_to_int = {value: key for key, value in tablex_gwis_object_types_int_to_str.items()}
-
-
-
-
 
 
 
