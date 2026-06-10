ec5bc33d8e1b1770819f928d773af510ae3c13e0
KeyisB <256410882+KeyisB@users.noreply.github.com>
2026-06-10 14:40:51 +0300

    refactor: simplify GWISObject initialization and update objectId method to use parameter

--- src/gnobjects/gwis/objects.py
+++ src/gnobjects/gwis/objects.py
@@ -2,13 +2,12 @@ from .values import tablex_gwis_object_types_int_to_str, table_gwis_object_types
 
 
 class GWISObject:
-    def __init__(self, gwisid: int, type: int):
+    def __init__(self, gwisid: int):
         self.gwisid = gwisid
-        self.type = type
         self.__object_id = None
 
-    @property
-    def objectId(self):
+    
+    def objectId(self, objectType: int):
         """
         # Получение id объекта
         ID объекта кодируется в gwisid
@@ -22,20 +21,34 @@ class GWISObject:
         if self.__object_id is not None:
             return self.__object_id
 
-        _r = table_gwis_object_types_int_ranges.get(self.type)
+        _r = table_gwis_object_types_int_ranges.get(objectType)
         if _r is None:
-            if self.type not in tablex_gwis_object_types_int_to_str:
-                raise Exception(f'Unknown object type {self.type}')
+            if objectType not in tablex_gwis_object_types_int_to_str:
+                raise Exception(f'Unknown object type {objectType}')
             else:
-                raise Exception(f'Object type {tablex_gwis_object_types_int_to_str[self.type]} has no supported object_id')
+                raise Exception(f'Object type {tablex_gwis_object_types_int_to_str[objectType]} has no supported object_id')
         
         object_id = self.gwisid - _r[0]
         if object_id < 0 or object_id >= _r[1] - _r[0]:
-            raise Exception(f'Invalid object_id {self.gwisid} for object type {tablex_gwis_object_types_int_to_str[self.type]}')
+            raise Exception(f'Invalid object_id {self.gwisid} for object type {tablex_gwis_object_types_int_to_str[objectType]}')
 
         self.__object_id = object_id
         return object_id
     
     @staticmethod
-    def fromObjectID(object_id: int, type: int):
-        return GWISObject(object_id + table_gwis_object_types_int_ranges[type][0], type)
+    def fromObjectID(object_id: int, objectType: int):
+        return GWISObject(object_id + table_gwis_object_types_int_ranges[objectType][0])
+
+    def toBytes(self) -> bytes:
+        """
+        # `2B type` `22B 0` `8B gwisid`
+
+        absid:
+        - [2B type] - 00
+        - [22B 0] - 0 * 22
+        - [8B gwisid] - gwisid
+        """
+        return bytes(24) + self.gwisid.to_bytes(8, 'big')
+
+    def fromBytes(self, data: bytes):
+        self.gwisid = int.from_bytes(data[2:10], 'big')
